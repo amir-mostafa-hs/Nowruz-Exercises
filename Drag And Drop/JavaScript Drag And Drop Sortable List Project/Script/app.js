c42aa4339bfa6e815ce5cc49bfc8480005d5cde0
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-04-09 23:52:05 +0430

    create random list

---
+++ Drag And Drop/JavaScript Drag And Drop Sortable List Project/Script/app.js
@@ -0,0 +1,42 @@
+const draggableList = document.querySelector("#draggableList");
+const checkBtn = document.querySelector("#check");
+const softwareCompanies = [
+  "Apple",
+  "Microsoft",
+  "Alphabet (Google)",
+  "Oracle",
+  "Adobe",
+  "Salesforce",
+  "Intuit",
+  "SAP",
+  "IBM",
+  "ServiceNow",
+];
+
+// store array
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// Insert list items into DOM
+function createList() {
+  [...softwareCompanies]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .forEach((person, index) => {
+      const listItem = document.createElement("li");
+      listItem.setAttribute("data-index", index);
+      listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+          <p class="person-name">${person.value}</p>
+          <i class="fas fa-grip-lines"></i>
+        </div>
+      `;
+      listItems.push(listItem);
+
+      draggableList.appendChild(listItem);
+    });
+}
