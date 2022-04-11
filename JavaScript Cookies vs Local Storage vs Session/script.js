899bd0d428d65a95579eb1fbe246fc938e496f0f
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-04-11 22:02:50 +0430

    Local Storage set and get methods were added

---
+++ JavaScript Cookies vs Local Storage vs Session/script.js
@@ -0,0 +1,13 @@
+// !Local Storage
+/*
+    localStorage method:
+     1- setItem : used to set the key and value of local storage .
+     2- getItem : used to get the value of the local storage key
+     3- removeItem : used to remove the local storage value.
+
+*/
+localStorage.setItem("Name", "AmirMostafa");
+
+const userName = localStorage.getItem("Name");
+
+console.log(`User Name is: ${userName}`);
