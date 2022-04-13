f27695b169e3ce030c56ca6edf1a897bf3e66b20
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-04-13 10:31:18 +0430

    Phone validation completed

--- Validate Form Using JavaScript/script.js
+++ Validate Form Using JavaScript/script.js
@@ -1,16 +1,25 @@
 const errors = document.querySelectorAll("span[id*='error']");
 
-function validateName(itemName) {
+function verification(itemName) {
   const item = document.querySelector(`#contact${itemName}`).value;
 
   if (itemName === "Name") {
-    // console.log(item);
     if (item.length === 0) {
       errors[0].textContent = "Name is required";
     } else if (!item.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
       errors[0].textContent = "Write full name";
     } else {
       errors[0].innerHTML = "<i class='fas fa-check-circle'></i>";
+    }
+  } else if (itemName === "Phone") {
+    if (item.length === 0) {
+      errors[1].textContent = "Phone is required";
+    } else if (item.length !== 10) {
+      errors[1].textContent = "Phone should be 10 digits";
+    } else if (!item.match(/^[0-9]{10}$/)) {
+      errors[1].textContent = "Only digits please";
+    } else {
+      errors[1].innerHTML = "<i class='fas fa-check-circle'></i>";
     }
   }
 }
