b0dffc715668aa5735663586abd55e7555699d7a
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-04-16 16:37:43 +0430

    Completed delete post session

--- Up and Running with JSON Server/script/details.js
+++ Up and Running with JSON Server/script/details.js
@@ -1,10 +1,10 @@
 const id = new URLSearchParams(window.location.search).get("id");
+const aliUrl = "http://localhost:3000/posts";
 const container = document.querySelector(".details");
+const deleteBtn = document.querySelector(".button");
 
 const rendetDetails = async () => {
-  const postsApiUrl = "http://localhost:3000/posts/";
-
-  const responseDetails = await (await fetch(postsApiUrl + id)).json();
+  const responseDetails = await (await fetch(`${aliUrl}/${id}`)).json();
 
   const title = document.createElement("h1");
   title.textContent = responseDetails.title;
@@ -14,5 +14,12 @@ const rendetDetails = async () => {
 
   container.append(title, textBody);
 };
+
+deleteBtn.addEventListener("click", async () => {
+  const response = await fetch(`${aliUrl}/${id}`, {
+    method: "DELETE",
+  });
+  window.location.replace("./index.html");
+});
 
 window.addEventListener("DOMContentLoaded", () => rendetDetails());
